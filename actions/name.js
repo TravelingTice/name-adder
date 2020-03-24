5dd15a2e399a29ac45fe483c5ec7b7115778ac70
Matthijs Kralt <matthijs29@live.com>
2020-03-24 10:37:57 +0100

    Add frontend for boilerplate

---
+++ actions/name.js
@@ -0,0 +1,18 @@
+import { config } from '../config/config';
+const API = config.appUrl + 'api';
+
+export const getNames = () => {
+  return fetch(`${API}/names`)
+  .then(res => res.json())
+  .catch(err => console.log(err))
+}
+
+export const addName = name => {
+  return fetch(`${API}/names`, {
+    method: 'POST',
+    ...config.jsonOptions,
+    body: JSON.stringify({ name })
+  })
+  .then(res => res.json())
+  .catch(err => console.log(err))
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,66 @@
+import { useState, useEffect } from "react";
+import { getNames, addName } from '../actions/name';
+
 const IndexPage = () => {
+  const [names, setNames] = useState([]);
+  const [values, setValues] = useState({
+    inputVal: '',
+    error: '',
+    success
+  });
+
+  const { inputVal, error, success } = values;
+
+  useEffect(() => {
+    getNames().then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setNames(data);
+      }
+    })
+  }, [])
+
+  const handleChange = name => e => {
+    setValues({ ...values, error: '', success: '', [name]: e.target.value });
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addName(inputVal).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, success: data.message });
+      }
+    });
+  }
+
+  const showNames = () => (
+    <div>
+      {JSON.stringify(names)}
+    </div>
+  )
+
+  const showError = () => error && <div className="alert alert-danger">{error}</div>
+  const showSuccess = () => success && <div className="alert alert-success">{success}</div>
+
   return (
-    <h1>Hi there</h1>
+    <>
+      <h1>Hi there</h1>
+      <p>Add your names below:</p>]
+
+      {showNames()}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="name" onChange={handleChange('inputVal')} value={inputVal} />
+        <input type="submit" value="Add"/>
+      </form>
+
+      {showError()}
+      {showSuccess()}
+    </>
   );
 }
 
